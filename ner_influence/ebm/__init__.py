e64f119af29c390d3d6f0c7b849751f1c4eb66b0
Sarthak Jain <jsarth@dev-dsk-jsarth-2a-b67d6f6c.us-west-2.amazon.com>
2022-12-03 07:05:45 +0000

    Remove Extra Code

--- ner_influence/ebm/__init__.py
+++ ner_influence/ebm/__init__.py
@@ -18,21 +18,3 @@ def load_datamodule(transformer=transformer):
     data.setup()
     data.set_validation_splits(["validation"])
     return data
-
-
-def load_datamodule_binarize(label):
-    splits = {
-        "train": "data/ebm_nlp/train.jsonl",
-        "validation": "data/ebm_nlp/dev.jsonl",
-        "test": "data/ebm_nlp/test.jsonl",
-    }
-
-    data = NERDataModule(
-        splits=splits,
-        label_list=["O", label],
-        transformer=transformer,
-        remap_labels={split: {x: "O" for x in ["POP", "INT", "OUT"] if x != label} for split in splits},
-    )
-    data.setup()
-    data.set_validation_splits(["validation"])
-    return data
